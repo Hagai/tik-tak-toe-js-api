333cd4a5f959a89f2ce922436c996a1a9835a4e4
Hagai <drory.hagai@gmail.com>
2016-09-17 10:53:06 +0300

    moved the requirements. small changes to run_server

--- src/tik_tak_toe_server/run_server.py
+++ src/tik_tak_toe_server/run_server.py
@@ -1,14 +1,22 @@
+import os
+
 import tornado.ioloop
 import tornado.web
 
 from tornado.options import define, options, parse_command_line
 
+
 define("port", default=8888, help="run on the given port", type=int)
 define("debug", default=False, help="run in debug mode")
 
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        html_to_send_path = os.path.join('static', 'tik-tak-toe.html')
+        with open(html_to_send_path) as f:
+            str_to_send = f.read()
+        self.write(str_to_send)
+        
 
 def make_app():
     app =  tornado.web.Application(
@@ -23,11 +31,14 @@ def make_app():
         )
     return app
 
+
 def main():
     parse_command_line()
     app = make_app()
     app.listen(options.port)
+    print("server listen on port: {options.port}".format(options=options))
     tornado.ioloop.IOLoop.current().start()
 
+
 if __name__ == "__main__":
-    main
+    main()
