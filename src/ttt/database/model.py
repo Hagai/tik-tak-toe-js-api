2cfde5913fa445f2ed227f4e17f69aa895b4ba75
Hagai <drory.hagai@gmail.com>
2016-10-03 19:29:21 +0300

    model: add relationship for Board model add players to Board init add name to Player init
    test basic model operation

--- src/ttt/database/model.py
+++ src/ttt/database/model.py
@@ -4,18 +4,25 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class Player(Base):
     __tablename__ = 'player'
     id = Column(Integer, primary_key=True)
     name = Column(String(64))
+    # board = relationship("Board", back_populates="player")
+
+    def __init__(self, name):
+        self.name = name
 
 
 class Board(Base):
     __tablename__ = 'board'
     id = Column(Integer, primary_key=True)
     name = Column(String(64))
-    player_x = Column(Integer, ForeignKey("player.id"))
-    player_y = Column(Integer, ForeignKey("player.id"))
+    player_x_id = Column(Integer, ForeignKey("player.id"))
+    player_y_id = Column(Integer, ForeignKey("player.id"))
+    player_x = relationship("Player", foreign_keys=[player_x_id])
+    player_y = relationship("Player", foreign_keys=[player_y_id])
     cell_1 = Column(String(1))
     cell_2 = Column(String(1))
     cell_3 = Column(String(1))
@@ -25,6 +32,8 @@ class Board(Base):
     cell_7 = Column(String(1))
     cell_8 = Column(String(1))
     cell_9 = Column(String(1))
-    
-    def __init__(self, name):
+
+    def __init__(self, name, player_x, player_y):
         self.name = name
+        self.player_x = player_x
+        self.player_y = player_y
--- src/ttt/test/test_db_.py
+++ src/ttt/test/test_db_.py
@@ -2,23 +2,76 @@ import unittest
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
-from models import Base, Panel
+from ttt.database import model
+from ttt.database.model import Base
 
 
-class TestQuery(unittest.TestCase):
+class _Helper:
+    @staticmethod
+    def add_two_players(session, player_1_name='prof X', player_2_name='O dear'):
+        player_1 = model.Player(player_1_name)
+        session.add(player_1)
+        player_2 = model.Player(player_2_name)
+        session.add(player_2)
+
+
+class TestPlayer(unittest.TestCase):
     def setUp(self):
         self.engine = create_engine('sqlite:///:memory:')
-        self.session = Session(engine)
+        self.session = Session(self.engine)
         Base.metadata.create_all(self.engine)
-        self.panel = Panel(1, 'ion torrent', 'start')
-        self.session.add(self.panel)
+        # self.session.add(self.panel)
         self.session.commit()
 
     def tearDown(self):
         Base.metadata.drop_all(self.engine)
 
-    def test_query_panel(self):
-        expected = [self.panel]
-        result = self.session.query(Panel).all()
-        self.assertEqual(result, expected)
+    def test_one(self):
+        session = Session(self.engine)
+        player_name = 'prof X'
+        player_1 = model.Player(player_name)
+        session.add(player_1)
+        session.commit()
+        players = session.query(model.Player)
+        player_1_get = players.one()
+        self.assertEqual(player_1_get.name, player_name)
+
+    def test_two(self):
+        session = Session(self.engine)
+        player_1_name = 'prof X'
+        player_1 = model.Player(player_1_name)
+        session.add(player_1)
+        player_2_name = 'O dear'
+        player_2 = model.Player(player_2_name)
+        session.add(player_2)
+        session.commit()
+        players = session.query(model.Player)
+        player_all = players.all()
+        expected = [player_1, player_2]
+        self.assertEqual(player_all, expected)
+
+
+class TestBoard(unittest.TestCase):
+    def setUp(self):
+        self.engine = create_engine('sqlite:///:memory:')
+        self.session = Session(self.engine)
+        Base.metadata.create_all(self.engine)
+        # self.session.add(self.panel)
+        # self.session.commit()
+
+    def tearDown(self):
+        Base.metadata.drop_all(self.engine)
+
+    def test_single_board(self):
+        session = Session(self.engine)
+        _Helper.add_two_players(session)
+        session.commit()
+        player_1, player_2 = session.query(model.Player).all()
+        board_name = 'board_test'
+        board = model.Board(board_name, player_1, player_2)
+        session.add(board)
+        session.commit()
+        board_result = session.query(model.Board).one()
+        self.assertEqual(board_result.name, board.name)
